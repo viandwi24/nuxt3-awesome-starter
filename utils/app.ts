4492dc180a373c7b7e64157746e27b3e070fb9c0
viandwi24 <fiandwi0424@gmail.com>
2022-01-15 05:15:27 +0700

    feat: add author to app state

--- utils/app.ts
+++ utils/app.ts
@@ -2,12 +2,20 @@ import { ThemeManager } from "./theme"
 
 export interface IApp {
   name: string
+  author: {
+    name: string
+    link: string
+  }
 }
 
 export function AppSetup() {
   // declare app information
   const app: IApp = {
-    name: 'Nuxt 3 Awesome Starter'
+    name: 'Nuxt 3 Awesome Starter',
+    author: {
+      name: 'viandwi24',
+      link: 'https://github.com/viandwi24'
+    }
   }
   useState('app', () => app)
 
