f6d678c426293a4cc5843a78e48e9e355b31407a
viandwi24 <fiandwi0424@gmail.com>
2022-01-18 04:45:50 +0700

    fix: default options show

--- plugins/navbar.ts
+++ plugins/navbar.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   // when page redirect on mobile device, close drawer navbar
   nuxtApp.hook('page:start', (page) => {
     const showDrawer = useState<boolean>('navbar.showDrawer', () => false)
-    const showOptions = useState<boolean>('navbar.showOptions', () => true)
+    const showOptions = useState<boolean>('navbar.showOptions', () => false)
     showDrawer.value = false
     showOptions.value = false
   })
