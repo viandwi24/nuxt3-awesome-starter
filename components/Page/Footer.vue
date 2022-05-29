fa7abcfe31f3754744c1d4e1f741146c33cfa1b0
viandwi24 <viandwicyber@gmail.com>
2022-05-29 15:24:29 +0700

    fix(footer): nuxt version text

--- components/Page/Footer.vue
+++ components/Page/Footer.vue
@@ -16,12 +16,14 @@ const app = useState<IApp>('app')
         <div class="text-xs text-gray-600 dark:text-gray-400">
           Copyright © 2022 <a :href="app.author.link">{{ app.author.name }}</a
           >. All rights reserved. Made with <span class="text-red-500">❤</span>
-          <div class="flex flex-col md:float-right">
+          <div
+            class="flex flex-col md:flex-row space-x-2 items-center md:float-right"
+          >
             <span class="text-center md:text-right">
               design by <a href="https://github.com/viandwi24">viandwi24</a>
             </span>
             <span
-              class="hidden md:block bg-blue-500 rounded px-1 py-0.5 text-white text-xs"
+              class="block bg-blue-500 rounded px-1 py-0.5 text-white text-xs"
             >
               {{ p.devDependencies.nuxt }}
             </span>
