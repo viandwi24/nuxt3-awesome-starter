ddf8416c56078161389ba7c73f6b77391c44bb47
viandwi24 <viandwicyber@gmail.com>
2022-08-12 16:03:26 +0700

    fix: error "Failed to execute 'insertBefore' on 'Node'"

--- utils/app.ts
+++ utils/app.ts
@@ -26,20 +26,6 @@ export function AppSetup() {
   // use language manager
   const languageManager = LanguageManager()
 
-  // vue transition bug handle
-  const messages = [
-    `Uncaught NotFoundError: Failed to execute 'insertBefore' on 'Node': The node before which the new node is to be inserted is not a child of this node.`, // chromium based
-    `NotFoundError: The object can not be found here.`, // safari
-  ]
-  if (typeof window !== 'undefined') {
-    window.addEventListener('error', (ev) => {
-      if (messages.includes(ev.message)) {
-        ev.preventDefault()
-        window.location.reload()
-      }
-    })
-  }
-
   // return
   return {
     app,
