cbdbc78e7ab46728a4bbbe80482f19812e2e693d
viandwi24 <viandwicyber@gmail.com>
2022-09-14 12:55:25 +0700

    feat: adding example test

---
+++ tests/example.test.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'node:url'
+import { describe, expect, it } from 'vitest'
+import { setup, $fetch, isDev } from '@nuxt/test-utils'
+
+describe('example', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    server: true,
+  })
+
+  it('Renders Nuxt 3 Awesome Starter', async () => {
+    expect(await $fetch('/')).toMatch('Nuxt 3 Awesome Starter')
+  })
+
+  if (isDev()) {
+    it('[dev] ensure vite client script is added', async () => {
+      expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
+    })
+  }
+})
