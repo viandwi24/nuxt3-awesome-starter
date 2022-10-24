3f51d4c1eef493fbcc6f03efac5df2f2f73a278b
viandwi24 <viandwicyber@gmail.com>
2022-10-24 11:55:30 +0700

    feat: adding loading indicator

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
       class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
     >
       <NuxtLayout>
+        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
         <NuxtPage />
       </NuxtLayout>
     </Body>
