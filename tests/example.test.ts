946ae142b8cddb01aa3000e2494bf9e90cf0e6fb
viandwi24 <viandwicyber@gmail.com>
2022-10-24 12:50:59 +0700

    fix: use test-utils-edge

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
-import { setup, $fetch, isDev } from '@nuxt/test-utils'
+import { setup, $fetch, isDev } from '@nuxt/test-utils-edge'
 
 describe('example', async () => {
   await setup({
