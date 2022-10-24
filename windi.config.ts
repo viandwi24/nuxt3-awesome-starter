a5e39e807408ee238f426a667059ca0412d531a1
viandwi24 <viandwicyber@gmail.com>
2022-10-24 13:04:32 +0700

    style(test): change primary color to blue

--- windi.config.ts
+++ windi.config.ts
@@ -64,7 +64,7 @@ export default defineConfig({
         '8xl': '90rem',
       },
       colors: {
-        primary: MyTheme.colors.green,
+        primary: MyTheme.colors.blue,
         // if want to change primary color to green
         // primary: MyTheme.colors.blue,
         green: MyTheme.colors.green,
