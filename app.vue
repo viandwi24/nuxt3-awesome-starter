7b1f0d1190ba796f18b85410564e6d7b9533e07d
weirui88888 <ruiwei88888@163.com>
2023-04-13 17:49:28 +0800

    style: 🌿 disable ios rubber band effect

--- app.vue
+++ app.vue
@@ -25,7 +25,7 @@ useHead({
 <template>
   <Html :class="`${theme === 'dark' ? 'dark' : ''}`" :lang="locale">
     <Body
-      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
+      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900 overscroll-y-none"
     >
       <NuxtLayout>
         <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
