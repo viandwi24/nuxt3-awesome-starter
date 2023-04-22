55d6e33558fca98446aaf90cf2dff43e73fea161
viandwi24 <viandwicyber@gmail.com>
2023-04-22 10:15:48 +0700

    style(button): add danger style

--- components/awesome/Button/index.vue
+++ components/awesome/Button/index.vue
@@ -37,6 +37,7 @@ const styles = reactive<{
     'text-slate-800 bg-gray-200 border-gray-200 hover:bg-gray-300 dark:text-white dark:border-slate-800 dark:bg-slate-800 dark:hover:bg-slate-700',
   opposite:
     'text-white bg-gray-800 hover:bg-white hover:text-gray-800 hover:border-gray-900 dark:text-gray-800 dark:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-100 dark:border-white',
+  danger: 'text-white bg-red-500 hover:bg-red-400 border-red-500',
 })
 const sizes = reactive<{
   [key: string]: string
