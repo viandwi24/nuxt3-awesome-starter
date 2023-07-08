b29d7774e456757d16b0047019524a8f5923e5bb
viandwi24 <viandwicyber@gmail.com>
2023-07-08 16:08:48 +0700

    chore: move example folder to .demo

---
+++ .demo/app.config.ts
@@ -0,0 +1,21 @@
+// see all availabe configs here: https://github.com/viandwi24/nuxt3-awesome-starter/blob/v2/app.config.ts
+export default defineAppConfig({
+  awesome: {
+    name: 'Nuxt 3 Awesome Starter Demo',
+    description:
+      'a demo from starter template for Nuxt 3 with minimalist themes design, built in components, drawer & menus, and more.',
+    author: {
+      name: 'viandwi24',
+      links: {
+        github: 'https://github.com/viandwi24',
+        medium: 'https://viandwi24.medium.com',
+        website: 'https://viandwi24.site',
+      },
+    },
+    project: {
+      links: {
+        github: 'https://github.com/viandwi24/nuxt3-awesome-starter',
+      },
+    },
+  },
+})
---
+++ .demo/nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: [
+    '@nuxt-awesome/theme',
+  ]
+})
