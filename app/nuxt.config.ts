d215ddc1253379611cd7a62e10239cb84bae6036
viandwi24 <viandwicyber@gmail.com>
2023-07-08 16:35:37 +0700

    chore: comment for USE_STATIC env

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,7 @@
 export default defineNuxtConfig({
   extends: '../',
+
+  // pass USE_STATIC to env when use "pnpm generate" for client only rendering support in @nuxt/content
   ssr: process.env.USE_STATIC ? false : undefined,
   nitro: process.env.USE_STATIC ? { preset: 'service-worker' } : undefined,
 })
